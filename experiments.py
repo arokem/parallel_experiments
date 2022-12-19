efa32f407375f1fc4ff1607d1dc058012c0ba7cc
Ariel Rokem <arokem@gmail.com>
2022-12-19 12:07:02 -0800

    Adds initial experiment.

--- experiments.py
+++ experiments.py
@@ -1,15 +1,36 @@
+import os.path as op
 import dipy.reconst.csdeconv as csd
 from dipy.data.fetcher import fetch_hcp
+from dipy.core.gradients import gradient_table
 import nibabel  as nib
-
+from dipy.data import dipy_home
+import time
 
 
 def main():
-    fetch_hcp(100307)
-    # gtab =
-    # csdm =
+    subject = 100307
+    fetch_dict = fetch_hcp(subject)
+    subject_dir = op.join(dipy_home,
+        "HCP_1200",
+        "derivatives",
+        "hcp_pipeline", "sub-{subject}", "ses-01",
+        )
+    subject_files = [op.join(subject_dir, "dwi",
+         "sub-{subject}_dwi.{ext}") for ext in ["nii.gz", "bval", "bvec"]]
+    data_img = nib.load(subject_files[0])
+    data = data_img.get_fdata()
+    seg_img = nib.load(op.join(
+        subject_dir, "anat", f'sub-{subject}_aparc+aseg_seg.nii.gz'))
+    seg_data = seg_img.get_fdata()
+    brain_mask = seg_data > 0
+    gtab = gradient_table(subject_files[1], subject_files[2])
 
-
+    csdm = csd.ConstrainedSphericalDeconvModel(gtab)
+    for engine in ["serial", "dask", "joblib", "ray"]:
+        print("Engine: ", engine)
+        t1 = time.time()
+        csdf = csdm.fit(data, mask=brain_mask, engine=engine)
+        print("Duration:", time.time() - t1, "seconds")
 
 if __name__ == "__main__":
     main()
