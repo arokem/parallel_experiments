728091dda5839e54924bbd1b00691ab098b14961
Ariel Rokem <arokem@gmail.com>
2022-12-19 11:49:36 -0800

    Initial commit.

---
+++ experiments.py
@@ -0,0 +1,15 @@
+import dipy.reconst.csdeconv as csd
+from dipy.data.fetcher import fetch_hcp
+import nibabel  as nib
+
+
+
+def main():
+    fetch_hcp(100307)
+    # gtab =
+    # csdm =
+
+
+
+if __name__ == "__main__":
+    main()
